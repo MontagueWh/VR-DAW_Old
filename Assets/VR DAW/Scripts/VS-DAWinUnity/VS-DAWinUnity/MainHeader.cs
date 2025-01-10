bf924a354cdd287a183e00d91352a22b780c2001
MontagueWh <130056175+MontagueWh@users.noreply.github.com>
2025-01-10 10:55:46 +0000

    10/1/2025 - Commit 1
    Mimicked the Tutorial video 'Episode 1 - Recording Audio' from the YouTube series 'Making A DAW In C#'. Finished the tutorial video, but might need a rewatch. Write and read functionality does not work for some reason??

--- Assets/VR DAW/Scripts/VS-DAWinUnity/VS-DAWinUnity/MainHeader.cs
+++ Assets/VR DAW/Scripts/VS-DAWinUnity/VS-DAWinUnity/MainHeader.cs
@@ -3,34 +3,13 @@ using System.Security.Cryptography;
 using NAudio;
 using NAudio.Wave;
 //using UnityEngine;
+using VS_DAWinUnity;
 
 namespace VS_DAWinUnity
 {
-    public class Class1
+    public class Main()
     {
-        public void Record()
-        {
-            double sampleRate = 44100.0d;
-
-            WaveInEvent wave = new WaveInEvent(); // recording audio data
-            wave.DeviceNumber = 0; // audio input 1
-            wave.WaveFormat = new WaveFormat((int)Math.Round(sampleRate), 16, 1);
-            wave.DataAvailable += AudioIn_DataAvailable;
-            wave.RecordingStopped += AudioIn_RecordingStopped;
-
-            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            filePath += "/Audio Recording";
-            WaveFileWriter writer = new WaveFileWriter(filePath, wave.WaveFormat);
-        }
-
-        private void AudioIn_DataAvailable(object? sender, WaveInEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void AudioIn_RecordingStopped(object? sender, StoppedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+        //Example example = new Example();
     }
 }
+
---
+++ Assets/VR DAW/Scripts/VS-DAWinUnity/VS-DAWinUnity/RecordingandPlayback.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+using NAudio;
+using NAudio.Wave;
+//using UnityEngine;
+using VS_DAWinUnity;
+
+namespace VS_DAWinUnity
+{
+    public class Class1
+    {
+        WaveFileWriter writer;
+        WaveInEvent wave;
+
+        /*public Example()
+        {
+            Record();
+        }*/
+
+        public void Record()
+        {
+            double sampleRate = 44100.0d;
+
+            wave = new WaveInEvent(); // recording audio data
+            wave.DeviceNumber = 0; // audio input 1
+            wave.WaveFormat = new WaveFormat((int)Math.Round(sampleRate), 16, 1);
+            wave.DataAvailable += AudioIn_DataAvailable;
+            wave.RecordingStopped += AudioIn_RecordingStopped;
+
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            filePath += "/Audio Recording.wav";
+            writer = new WaveFileWriter(filePath, wave.WaveFormat);
+        }
+
+        // Setup Unity to stop recording when a 'stop' button is pressed
+        public void StopRecording()
+        {
+
+        }
+
+        private void AudioIn_DataAvailable(object? sender, WaveInEventArgs e)
+        {
+            writer.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private void AudioIn_RecordingStopped(object? sender, StoppedEventArgs e)
+        {
+            writer.Dispose();
+        }
+    }
+}
